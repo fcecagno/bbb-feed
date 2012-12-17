84887d2a89c0c441a85a3100b8ea909bbd0d53e0
Felipe Cecagno <fcecagno@gmail.com>
2012-12-17 16:49:19 -0200

    a working version already with the hub link; it's still needed to implement the interaction with the hub

--- app.js
+++ app.js
@@ -3,18 +3,13 @@ http://tbranyen.com/post/generating-rss-feeds-from-a-backbonejs-collection-in-no
 */
 
 var redis = require('redis');
-var sys = require('sys');
-var net = require('net');
-var RSS = require('rss');
-var xml2js = require('xml2js');
+var ATOM = require('atom');
 var Backbone = require('backbone');
 var express = require('express');
-var http = require('http');
-var app = express();
 
-var limit = 30;
+var limit = 50;
 var port = 3000;
-var serverUrl = "http://143.54.10.215:" + port;
+var app = express();
 
 var Post = Backbone.Model.extend({
 	idAttribute: "slug",
@@ -28,29 +23,32 @@ var Post = Backbone.Model.extend({
 	}
 });
 
-var xmlParser = new xml2js.Parser();
-
 var Posts = Backbone.Collection.extend({
 	model: Post,
+	cache: null,
 
-/*
-	comparator: function(post) {
-		return post.get("date");
-	},
-*/
 	sync: function(method, model, options) {
 
 	},
-// <atom:link rel="hub" href="http://pubsubhubbub.appspot.com"/>
+
 	initialize: function() {
-		this.feed = new RSS({
+		this.feed = new ATOM({
 			title: "BigBlueButton Events Feed",
 			description: "This is the RSS Feed for all the events passing through Redis",
-			feed_url: serverUrl + "/rss.xml",
-			site_url: serverUrl,
 			image_url: "https://lh6.googleusercontent.com/-Yy3PkhA5aIM/TrxTxnoqxkI/AAAAAAAAACw/D8IK2UEAmjE/s414/bbb_logo.jpg",
 			author: "BigBlueButton",
+			hub_url: "http://pubsubhubbub.appspot.com"
 		});
+	},
+
+	invalidateCache: function() {
+		this.cache = null;
+	},
+
+	xml: function() {
+		if (this.cache === null)
+			this.cache = this.feed.xml(true);
+		return this.cache;
 	}
 });
 
@@ -69,13 +67,14 @@ sub.on("message", function(channel, message) {
 	var title = channel + "-" + timestamp;
 
 	posts.feed.item({
-		title: channel + "-" + timestamp,
+		title: title,
+		author: channel,
 		description: message,
-		date: timestamp,
-		url: posts.feed.feed_url + "?id=" + title
+		updated: timestamp,
+		url: "/rss.xml?id=" + title
 	});
-
-	//sys.debug(JSON.stringify(posts.feed.items));
+	posts.invalidateCache();
+	//console.log(JSON.stringify(posts.feed.items));
 
 	// limit maximum number of feeds
 	if (posts.feed.items.length > limit)
@@ -84,11 +83,17 @@ sub.on("message", function(channel, message) {
 
 sub.subscribe("bigbluebutton:meeting:participants");
 sub.subscribe("bigbluebutton:meeting:system");
-sub.subscribe("bigbluebutton:meeting:presentation");
+//sub.subscribe("bigbluebutton:meeting:presentation");
 
 app.get("/rss.xml", function(req, res) {
-	res.contentType("rss");
-	res.send(posts.feed.xml());
+
+	if (posts.feed.feed_url === undefined) {
+		posts.feed.site_url = "http://" + req['headers']['host'];
+		posts.feed.feed_url = "http://" + req['headers']['host'] + "/rss.xml";
+	}
+
+	res.contentType("atom");
+	res.send(posts.xml());
 });
 
 app.use(function(err, req, res, next){
